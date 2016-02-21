c2c27e12dc3377c2a2b3766030ec4877fe1a5a04
Sergii Ladniuk <sergii.ladniuk@gmail.com>
2016-02-21 15:42:41 -0500

    fix rss

--- scripts/lib/render-all/render-rss.js
+++ scripts/lib/render-all/render-rss.js
@@ -1,11 +1,38 @@
 var renderTemplate = require('./render-template');
 var saveContent = require('./save-content');
+var RSS = require('rss');
 
-const renderRss = module.exports =  function (data) {
+const renderRss = module.exports = function (data) {
     var content = {
-        posts: data.list.filter(function(post) {
+        posts: data.list.filter(function (post) {
             return !post.draft && post.meta.type === 'post';
         })
     };
-    return saveContent(data, renderTemplate('rss.jade', data, content),0, null, 'xml', 'feed');
+
+    var feedOpts = {
+        title: 'marinatravelblog.com',
+        site_url: 'http://marinatravelblog.com',
+        description: 'Наша кругосветка и другие самостоятельные путешествия по Европе, Азии, США и Латинской Америке!',
+        language: 'ru-RU',
+        feed_url: 'http://marinatravelblog.com/feed.xml'
+    };
+
+    var feed = new RSS(feedOpts);
+
+    data.list.filter(function (post) {
+        return !post.draft && post.meta.type === 'post';
+    }).forEach(function (post) {
+        feed.item({
+            title: post.meta.title,
+            description: post.summary.replace(/<div.*?>[^]*?<noscript.*?>([^]*?)<\/noscript>[^]*?<\/div>/g, '$1')
+            + '<a href="' + post.meta.link + '">' + post.more + '</a>',
+            url: post.meta.link,
+            guid: post.meta.id,
+            categories: post.meta.categories,
+            date: post.meta.publishedDate
+        });
+    });
+
+    //return saveContent(data, renderTemplate('rss.jade', data, content), 0, null, 'xml', 'feed');
+    return saveContent(data, feed.xml({indent: false}), 0, null, 'xml', 'feed');
 };
