2669d8944f2c91aefaa666a3126c6cb54cfc0862
Sergii Ladniuk <sergii.ladniuk@gmail.com>
2016-03-10 06:42:44 -0700

    fix menu on touch devices

--- static/js/easyweb.js
+++ static/js/easyweb.js
@@ -1,9 +1,20 @@
 $(function () {
     console.log($(window).height());
-    console.log($(document).height())
+    console.log($(document).height());
     if (($(window).height() + 100) < $(document).height()) {
         $('#top-link-block').removeClass('hidden').affix({
             offset: {top: 100}
-        });
+        })
+    }
+
+    if (is_touch_device()) {
+        $('.dropdown-toggle.disabled').each(function(index, item) {
+            $(item).removeClass('disabled');
+        })
     }
 });
+
+function is_touch_device() {
+    return 'ontouchstart' in window        // works on most browsers
+        || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+}
