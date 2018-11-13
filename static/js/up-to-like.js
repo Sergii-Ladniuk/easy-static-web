982b8e41cf21dca126941bc4629df8fdefdac679
Sergii Ladniuk <sergii.ladniuk@gmail.com>
2018-11-13 20:06:03 +0300

    subscribe buttons and banners

--- static/js/up-to-like.js
+++ static/js/up-to-like.js
@@ -4,13 +4,28 @@ $(function () {
         $('#social-btns-side').hide();
 
         $(window).scroll(function () {
-            if (isScrolledIntoView($('#social-btns-bottom')) || isScrolledIntoView($('#mainMenu'))) {
+            if ((isScrolledIntoView($('#social-btns-bottom')) || isScrolledIntoView($('#mainMenu')))
+                && $(window).width > 900) {
+
                 $('#social-btns-side').fadeOut("slow");
             } else {
                 $('#social-btns-side').fadeIn("slow");
             }
-        });
 
+            if ($(window).scrollTop() > 100) {
+                $('#sidebar').css({top: '0'});
+            } else {
+                $('#sidebar').css({top: '75px'});
+            }
+
+            let subscribePopupMargin = $(document).height() / 2;
+            if ($(window).scrollTop() > subscribePopupMargin) {// && !localStorage.subscribeShown) {
+                localStorage.subscribeShown = true;
+                var subscribePopup = $('#subscribe-popup');
+                subscribePopup.modal('show');
+                subscribePopup.removeClass('hide')
+            }
+        });
 
         function isScrolledIntoView(elem) {
             var $elem = $(elem);
@@ -22,7 +37,30 @@ $(function () {
             var elemTop = $elem.offset().top;
             var elemBottom = elemTop + $elem.height();
 
-            return ((elemTop-100 <= docViewBottom) && (elemBottom+100 >= docViewTop));
+            return ((elemTop - 100 <= docViewBottom) && (elemBottom + 100 >= docViewTop));
         }
     }
+
+    $('#mobile-subscribe-btn').click(function (e) {
+        var $subscribe = $('#subscribe-modal');
+        $subscribe.modal('show');
+        $subscribe.removeClass('hide');
+        e.preventDefault();
+    });
+    $('#mobile-share-btn').click(function (e) {
+        var $share = $('#share-modal');
+        $share.modal('show');
+        $share.removeClass('hide');
+        e.preventDefault();
+    });
+    $('#subscribe-modal .subscribe-link').click(function () {
+        var modal = $('#subscribe-modal');
+        modal.modal('hide');
+        modal.addClass('hide');
+    });
+    $('#share-modal a, #share-modal a span').click(function () {
+        var modal = $('#share-modal');
+        modal.modal('hide');
+        modal.addClass('hide');
+    });
 });
